7ab2b503647f75078ac6f3aef405c337c1b9c6a5
dkb18 <dkbhoora01@gmail.com>
2022-06-09 00:42:46 +0200

    Convoluted NN Model on MNIST dataset

--- data_formatter.py
+++ data_formatter.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ast
 import math
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 
 #convert decimal rgb to rgb-255
 def evaluateValues(arr):
@@ -28,7 +33,7 @@ def formatData():
             imgs += [generateImage(str)]
 
     f.close()
-    return (imgs)
+    return np.array(imgs)
 
 #refine data to a 28x28 grid
 def refineData(imgs):
@@ -43,15 +48,19 @@ def refineData(imgs):
                 for i in range(3):
                     ans=ans+ math.pow(img[r][c][i],2)
                 ans=ans/3
-                Two_Dim[r][c]= math.floor(math.sqrt(ans))
+                Two_Dim[r][c]= math.floor(math.sqrt(ans)/255)
         refinedImgs += [Two_Dim]
+        
+    refinedImgs = np.reshape(refinedImgs,(len(imgs),784));
 
     return(refinedImgs)
 
 if __name__ == "__main__":
     imgs = formatData()
+    
+    print(imgs[2].shape)
     refinedImgs = refineData(imgs)
-    print(len(imgs))
+    print(len(refinedImgs[2]))
     inp = int(input('enter Image imgber to view: '))
 
     # while (inp != -1):
