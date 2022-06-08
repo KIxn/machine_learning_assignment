55ebc731d251b3adedcc0ed193baee589c211480
Kian Reddy <kianreddy67@gmail.com>
2022-06-09 01:58:41 +0200

    testing formatter

--- data_formatter.py
+++ data_formatter.py
@@ -8,13 +8,15 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+
 #convert decimal rgb to rgb-255
 def evaluateValues(arr):
     vals = []
     for x in arr:
         vals += [math.floor(ast.literal_eval(x) * 255.0)]
 
-    return(np.array(vals))
+    return (np.array(vals))
+
 
 def generateImage(str):
     arr = np.array(str.split(" "))
@@ -35,31 +37,54 @@ def formatData():
     f.close()
     return np.array(imgs)
 
+
 #refine data to a 28x28 grid
 def refineData(imgs):
     refinedImgs = []
     for img in imgs:
-        Two_Dim=np.full((28, 28), 0)
+        Two_Dim = np.full((28, 28), 0)
         for r in range(28):
 
             for c in range(28):
-                ans=0
+                ans = 0
 
                 for i in range(3):
-                    ans=ans+ math.pow(img[r][c][i],2)
-                ans=ans/3
-                Two_Dim[r][c]= math.floor(math.sqrt(ans)/255)
+                    ans = ans + math.pow(img[r][c][i], 2)
+                ans = ans / 3
+                Two_Dim[r][c] = math.floor(math.sqrt(ans) / 255)
         refinedImgs += [Two_Dim]
-        
-    refinedImgs = np.reshape(refinedImgs,(len(imgs),784));
 
-    return(refinedImgs)
+    refinedImgs = np.reshape(refinedImgs, (len(imgs), 784))
+
+    return (refinedImgs)
+
+
+#take in labels
+def formatLabels():
+    imgs = []
+    #take in data
+    with open('labels.txt', 'r') as f:
+        while True:
+            str = f.readline()
+            if not str:
+                break
+            imgs += [generateImage(str)]
+
+    f.close()
+    return np.array(imgs)
+
 
 if __name__ == "__main__":
     imgs = formatData()
-    
+
     print(imgs[2].shape)
     refinedImgs = refineData(imgs)
+    t = torch.tensor(refinedImgs)
+    tl = torch.tensor(formatLabels())
+    print(type(t))
+    print(t.shape)
+    print(type(tl))
+    print(tl.shape)
     print(len(refinedImgs[2]))
     inp = int(input('enter Image imgber to view: '))
 
